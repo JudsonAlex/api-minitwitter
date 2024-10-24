bab650c3a7aa7fc273bd4968b06b86ce33c23c61
Judson <pequenojudson@gmail.com>
2024-10-23 22:20:34 -0300

    create serializer and views for  User

--- minitwitter/settings.py
+++ minitwitter/settings.py
@@ -104,6 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 AUTH_USER_MODEL = 'users.User'
 
 
