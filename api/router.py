fdd8a595416525129c13e0dfc0533ae1e2cfed05
Judson <pequenojudson@gmail.com>
2024-10-23 22:17:07 -0300

    create api router

---
+++ api/router.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from users import urls as users_urls
+from posts import urls as posts_urls
+from users.serializer import MyTokenObtainPairView
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+api_urlpatterns = [
+    path('users/', include(users_urls), name='users'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair')
+    
+
+]
+
+app_name = "api"
+urlpatterns = router.urls
+urlpatterns += api_urlpatterns
