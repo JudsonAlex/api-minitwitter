997c901c91ea6034a7a5b76310dcbcaa79fd2833
Judson <pequenojudson@gmail.com>
2024-10-27 17:28:55 -0300

    alter delete route

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from .views import UserViewSet, FollowViewSet
 app_name = 'users'
 
 urlpatterns = [
-    path('', UserViewSet.as_view({'post': 'create', 'get': 'list'}), name='users'),
-    path('follows/', FollowViewSet.as_view({'post': 'create'}), name='follow-create'),
-    path('follows/delete', FollowViewSet.as_view({'post': 'destroy'}), name='follow-delete')
+    path('', UserViewSet.as_view({'post': 'create', 'get': 'list'}), name='user'),
+    path('follows/', FollowViewSet.as_view({'post': 'create', 'delete': 'destroy'}), name='follow'),
+    # path('follows/delete', FollowViewSet.as_view({'delete': 'destroy'}), name='follow')
 ]
